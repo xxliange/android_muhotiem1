5cb3494f170c1271c4150514f41ef19f2e0bba17
xiaoliange <353673246@qq.com>
2020-06-23 13:36:17 +0800

    完成密码登陆样式布局

--- app/src/main/java/com/example/muhoitem1/ui/activity/mineActivity/LoginActivity.java
+++ app/src/main/java/com/example/muhoitem1/ui/activity/mineActivity/LoginActivity.java
@@ -1,16 +1,16 @@
 package com.example.muhoitem1.ui.activity.mineActivity;
 
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.muhoitem1.R;
 import com.example.muhoitem1.base.BaseActivity;
-import com.example.muhoitem1.utils.ToastUtils;
 
 import butterknife.BindView;
 
 public class LoginActivity extends BaseActivity {
+    @BindView(R.id.tool_title)
+    public TextView toolTitle;
     public ImageView back;
     @Override
     protected void initView() {
@@ -19,6 +19,7 @@ public class LoginActivity extends BaseActivity {
             finish();
         });
         // TODO:
+        toolTitle.setText("密码登陆");
     }
 
     @Override
