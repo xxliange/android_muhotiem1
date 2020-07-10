3d2b9265a0d0079c64f2cc1b6119093d18ef8f22
xiaoliange <353673246@qq.com>
2020-07-10 14:06:35 +0800

    fix code

--- app/src/main/java/com/example/muhoitem1/ui/activity/testActivity/TestActivity.java
+++ app/src/main/java/com/example/muhoitem1/ui/activity/testActivity/TestActivity.java
@@ -8,15 +8,18 @@ import android.graphics.drawable.TransitionDrawable;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.muhoitem1.R;
 import com.example.muhoitem1.base.BaseActivity;
 import com.example.muhoitem1.ui.activity.mineActivity.LoginActivity;
+import com.example.muhoitem1.utils.Utils;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -30,6 +33,8 @@ public class TestActivity extends BaseActivity {
     public ImageView imageView;
     @BindView(R.id.test_btn)
     public TextView btn;
+    @BindView(R.id.test_closeBtn)
+    public ImageView test_closeBtn;
     private int isTran = 1;
     Timer timer = new Timer();
 
@@ -62,6 +67,7 @@ public class TestActivity extends BaseActivity {
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
         });
+        test_closeBtn.setOnClickListener(v -> finish());
     }
 
     private void transistionImg() {
@@ -92,6 +98,10 @@ public class TestActivity extends BaseActivity {
 
     @Override
     protected void initView() {
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(test_closeBtn.getLayoutParams());
+        layoutParams.setMargins(40,Utils.getStatusBarHeight(this)+20,0,0);
+        test_closeBtn.setLayoutParams(layoutParams);
+
         setTransparentStatusBar();
         imageView.setImageResource(R.drawable.bg1);
         timerRun();
